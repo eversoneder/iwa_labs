73683ecf1e4d278de201f8f612644d9af44599cc
eversoneder <everson_spinola@hotmail.com>
2022-04-08 09:09:51 +0000

    routes.js

--- models/image.js
+++ models/image.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-var imageSchema = new mongoose.Schema({
+const imageSchema = new mongoose.Schema({
     filename: String,
     originalName: String,
     desc: String,
     created: Date
-    });
+});
 
 module.exports = mongoose.model('Image', imageSchema);
--- routes.js
+++ routes.js
@@ -1,7 +1,11 @@
+module.exports.UPLOAD_PATH = 'uploads';
+
 const   express = require("express"),
         router = express.Router(),
-        itemCtrl = require("./item-controller"),
-        userCtrl = require("./user-controller");
+        imageCtrl = require('./image-controller'),
+        userCtrl = require("./user-controller"),
+        multer = require('multer'),
+        upload = multer({ dest: module.exports.UPLOAD_PATH });
 
 router.post('/users', userCtrl.createUser);
 router.get('/users', userCtrl.getUsers);
@@ -11,6 +15,7 @@ router.delete('/users/:id', userCtrl.deleteUser);
 
 router.post('/images', upload.single('image'), imageCtrl.uploadImage);
 router.get('/images', imageCtrl.getImages);
-router.get('/images/:id', imageCtrl.deleteImage);
+router.get('/images/:id', imageCtrl.getImage);
+router.delete('/images/:id', imageCtrl.deleteImage);
 
 module.exports = router;
