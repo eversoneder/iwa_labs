c9709a67898c63671cabbb13a31684ae501351f0
eversoneder <everson_spinola@hotmail.com>
2022-05-20 17:35:15 +0000

    server js refactor

--- src/server.js
+++ src/server.js
@@ -1,28 +1,30 @@
-const express = require('express');          // Node.JS Backend Framework
-const app = express()
+const express = require('express');//node js backend framework
+const dotenv = require('dotenv');//db environment var
 
-//make express to use json data format
+
+//make express to use json data format instead of using body-parser
+const app = express();
 app.use(express.json());
 
-// Log All Requests
+//Log requests
 const morgan = require('morgan');
 app.use(morgan('tiny'));
 
-// Load Routers
-app.use(require('./api/routes/router'))        // API
+// Load CRUD Routes
+app.use(require('./api/routes/router'))// API
 
 
 // Setup Front End
 const path = require('path');
-app.set("view engine", "ejs")
-app.set("views", path.resolve(__dirname, "frontend/pages"))
+app.set("view engine", "ejs");//html template engine
+app.set("views", path.resolve(__dirname, "frontend/pages"));
 
 // Load Assets
 app.use('/css', express.static(path.resolve(__dirname, "frontend/assets/css")));
 app.use('/js', express.static(path.resolve(__dirname, "frontend/assets/js")));
 // app.use('/img', express.static(path.resolve(__dirname, "frontend/assets/img")));
 
-const port = 3000
-app.listen(port, function(err) {
-  console.log(`Server Listening on Port: ${port}`)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(err) {
+  console.log(`Server Listening on Port: ${PORT}`)
 });
