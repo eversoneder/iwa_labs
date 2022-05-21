b31b48278b56ad33e79e42b966d41849d97e65e6
eversoneder <everson_spinola@hotmail.com>
2022-05-21 20:16:53 +0000

    fixing server.js

--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
-const express = require('express');//node js backend framework
-const dotenv = require('dotenv');//db environment var
+const express = require('express'),//node js backend framework
+      dotenv = require('dotenv'),//db environment var
+      morgan = require('morgan'),//req logger
+      path = require('path');//path directory
 
 
-//make express to use json data format instead of using body-parser
+// Make express to use json data format instead of using body-parser
 const app = express();
 app.use(express.json());
 
-//Log requests
-const morgan = require('morgan');
+// Log requests
 app.use(morgan('tiny'));
 
 // Load CRUD Routes
-app.use(require('./api/routes/router'))// API
+app.use(require('./api/routes/router'))// back-end route
 
 
 // Setup Front End
-const path = require('path');
 app.set("view engine", "ejs");//html template engine
 app.set("views", path.resolve(__dirname, "frontend/pages"));
 
@@ -24,6 +24,7 @@ app.use('/css', express.static(path.resolve(__dirname, "frontend/assets/css")));
 app.use('/js', express.static(path.resolve(__dirname, "frontend/assets/js")));
 // app.use('/img', express.static(path.resolve(__dirname, "frontend/assets/img")));
 
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(err) {
   console.log(`Server Listening on Port: ${PORT}`)
